83b3d3b6e72b3160d1220426742615d48bb45427
AnnaMille <62601095+AnnaMille@users.noreply.github.com>
2025-01-06 15:31:16 +0100

    Major changes
    Removed redundancy. Loops changed to dplyr. Added regionmap

--- EpiLinx git/Functions/Matrix4Viz.R
+++ EpiLinx git/Functions/Matrix4Viz.R
@@ -9,19 +9,17 @@ Matrix4Viz <- function(dt, names, dates, location) {
   Mat <- matrix(data=NA, nrow=length(names), ncol = length(dates))
   rownames(Mat) <- names
   colnames(Mat) <- dates
-
+    
     for (i in  1:nrow(dt)){
       tempDays=seq(min(as.Date(dt$InDate[i])),max(as.Date(dt$OutDate[i])), "day")
       colIndx=which(dates %in% tempDays)
       rowIndx =which(names %in% dt$patient[i])
       Mat[rowIndx,colIndx] = dt[[location]][i]
     }
-
+    
     # Filter non-admitted patients
-  ResMat <- Mat[rowSums(is.na(Mat))!=ncol(Mat),]
-  if(nrow(Mat) == 1)   ## KTKA: Added for single row matrices
-    ResMat <- t(Mat[rowSums(is.na(Mat))!=ncol(Mat),])
-  colnames(ResMat) <- format(as.Date(dates, format= "%Y-%m-%d"), format= "%Y-%m-%d")
-
+    ResMat <- Mat[rowSums(is.na(Mat))!=ncol(Mat),]
+    colnames(ResMat) <- format(as.Date(dates, format= "%Y-%m-%d"), format= "%Y-%m-%d")
+    
   return(ResMat)
 }
