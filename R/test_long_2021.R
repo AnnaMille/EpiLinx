baddf05a76fa6057f05ea1015b145bf44eb3da2e
Matt Denwood <md@sund.ku.dk>
2025-02-07 18:20:40 +0100

    Clean up more warnings/notes

--- R/test_long_2021.R
+++ R/test_long_2021.R
@@ -5,7 +5,36 @@
 #' @format
 #' A data frame with:
 #' \describe{
-#'   \item{SSI_id}{Anonymised ID}
-#'   ...
+#'   \item{CPR_male}{TODO: description}
+#'   \item{C_STATUS}{TODO: description}
+#'   \item{CarbapenemaseNGS}{TODO: description}
+#'   \item{D_STATUS_HEN_START}{TODO: description}
+#'   \item{GeografiskLokalisation}{TODO: description}
+#'   \item{IsolatnrPat}{TODO: description}
+#'   \item{KMA}{TODO: description}
+#'   \item{Lokalnr}{TODO: description}
+#'   \item{MLST1}{TODO: description}
+#'   \item{MuligPatientUdbrudsnr}{TODO: description}
+#'   \item{MuligeUdbrud}{TODO: description}
+#'   \item{Proveart}{TODO: description}
+#'   \item{Provedato}{TODO: description}
+#'   \item{Region}{TODO: description}
+#'   \item{Regionkode}{TODO: description}
+#'   \item{Rejse}{TODO: description}
+#'   \item{SSI_id}{TODO: description}
+#'   \item{SorIdentifier}{TODO: description}
+#'   \item{Species}{TODO: description}
+#'   \item{Ward}{TODO: description}
+#'   \item{alder}{TODO: description}
+#'   \item{cgMLST}{TODO: description}
+#'   \item{course_id}{TODO: description}
+#'   \item{dept_ind_dato}{TODO: description}
+#'   \item{dept_ud_dato}{TODO: description}
+#'   \item{hospitalCPO}{TODO: description}
+#'   \item{hospital_name}{TODO: description}
+#'   \item{leave}{TODO: description}
+#'   \item{name}{TODO: description}
+#'   \item{responsible_name}{TODO: description}
+#'   \item{source}{TODO: description}
 #' }
 "test_long_2021"
